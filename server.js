6309a5b5232d023dca494434fa042a6ba5bd03cb
Abdullah-webd <ajalabdllh@gmail.com>
2025-08-05 14:28:07 +0100

    second commit

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use('*', (req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-const PORT = 2000;
-app.listen(2000, () => {
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
